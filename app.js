42f422b1b33846cc0a227340e0d498a93f1ab349
FFiillaattoovv <admirator2015@yandex.ru>
2021-12-07 20:32:40 +0300

    Add base routing

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const PORT = 3000
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log('Server request')
-    res.setHeader('Content-Type', 'application-json')
+    res.setHeader('Content-Type', 'text-html');
 
-    const data = JSON.stringify([
-        {name: 'Bob', age: 27},
-        {name: 'John', age: 19},
-    ])
+    const createPath = (page) => {
+        return path.resolve(__dirname, 'views', `${page}.html`);
+    };
 
-    res.end(data)
-})
+    let basePath = '';
+
+    switch (req.url) {
+        case '/':
+            basePath = createPath('index');
+            res.statusCode = 200;
+            break;
+        case '/contacts':
+            basePath = createPath('contacts');
+            res.statusCode = 200;
+            break;
+        default:
+            basePath = createPath('error');
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(basePath, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.statusCode = 500;
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
+});
 
 server.listen(PORT, 'localhost', (error) => {
-    error ? console.log(error) : console.log(`Listening port ${PORT}`)
-})
+    error ? console.log(error) : console.log(`Listening port ${PORT}`);
+});
