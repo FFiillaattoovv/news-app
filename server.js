69b44d323281f4c2c111ee2488fb760f002f0a4d
FFiillaattoovv <admirator2015@yandex.ru>
2021-12-08 13:01:58 +0300

    Add EJS

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const PORT = 3000;
 
 const createPath = (page) => {
-    return path.resolve(__dirname, 'views', `${page}.html`);
+    return path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 };
 
 app.listen(PORT, 'localhost', (error) => {
@@ -14,15 +16,45 @@ app.listen(PORT, 'localhost', (error) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(createPath('index'));
+    const title = 'Home';
+
+    res.render(createPath('index'), {title});
 });
 
 app.get('/contacts', (req, res) => {
-    res.sendFile(createPath('contacts'));
+    const title = 'Contacts';
+
+    const contacts = [
+        {name: 'Telegram', link: 'https://t.me/FFiillaattoovv'},
+        {name: 'LinkedIn', link: 'https://www.linkedin.com/in/FFiillaattoovv'},
+        {name: 'GitHub', link: 'https://github.com/FFiillaattoovv'},
+    ];
+
+    res.render(createPath('contacts'), {contacts, title});
+});
+
+app.get('/posts/:id', (req, res) => {
+    const title = 'Post';
+
+    res.render(createPath('post'), {title});
+});
+
+app.get('/posts', (req, res) => {
+    const title = 'Posts';
+
+    res.render(createPath('posts'), {title});
+});
+
+app.get('/add-post', (req, res) => {
+    const title = 'Add post';
+
+    res.render(createPath('add-post'), {title});
 });
 
 app.use((req, res) => {
+    const title = 'Error page';
+
     res
         .status(404)
-        .sendFile(createPath('error'));
+        .render(createPath('error'), {title});
 });
