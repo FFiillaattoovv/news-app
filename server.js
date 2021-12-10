c94b9461f431da518b6f410b0974e880c497d73a
FFiillaattoovv <admirator2015@yandex.ru>
2021-12-10 17:38:21 +0300

    Add settings for deploy on Heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const methodOverride = require('method-override');
 const postRoutes = require('./routes/post-routes');
 const postApiRoutes = require('./routes/api-post-routes');
@@ -15,16 +16,13 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-const PORT = 3000;
-const db = 'mongodb+srv://Petrov:007Database@cluster0.bydz4.mongodb.net/news-app?retryWrites=true&w=majority';
-
 mongoose
-    .connect(db)
+    .connect(process.env.MONGO_URL)
     .then(() => console.log(successMsg('Connected to DB')))
     .catch(err => console.log(errorMsg(err)));
 
-app.listen(PORT, 'localhost', (error) => {
-    error ? console.log(errorMsg(error)) : console.log(successMsg(`Listening port ${PORT}`));
+app.listen(process.env.PORT, 'localhost', (error) => {
+    error ? console.log(errorMsg(error)) : console.log(successMsg(`Listening port ${process.env.PORT}`));
 });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
